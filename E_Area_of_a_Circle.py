739b39f11fc766d1d2f7dc3750be7cf9d8c956b2
Aadyanth Krishna A <68956013+aadh47@users.noreply.github.com>
2026-09-11 22:14:54 +0530

    Did some problems

---
+++ E_Area_of_a_Circle.py
@@ -0,0 +1,4 @@
+r=float(input())
+pi=3.141592653
+area=pi*(r**2)
+print(area)
---
+++ G_Summation_from_1_to_N.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+
+int main() {
+    long long y;
+
+    std::cin>>y;
+    long long x=y*(y+1)/2;
+    std::cout<<x;
+    return 0;
+}
